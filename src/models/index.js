1437e44e6376030fda8632b9fd13877c5dff7eac
Jesus Guerrero <rocha.jesus@hotmail.com>
2017-10-03 22:16:55 -0700

    Adding filtering by search

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,13 @@
 import { Notice } from './notification.model';
 import { Article } from './article.model';
+import FilterModel from './filters/filter.model';
+import NoFilter from './filters/no-filter.model';
+import SearchByNameFilter from './filters/search-by-name-filter.model';
 
 export {
   Notice,
-  Article
+  Article,
+  FilterModel,
+  NoFilter,
+  SearchByNameFilter,
 }
