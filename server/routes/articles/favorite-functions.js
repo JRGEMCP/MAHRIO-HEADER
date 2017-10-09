0fd3c0ea1204540442d8796bd703cab98f3b728e
Jesus Guerrero <rocha.jesus@hotmail.com>
2017-10-08 17:16:11 -0700

    server add user favorites

---
+++ server/routes/articles/favorite-functions.js
@@ -0,0 +1,29 @@
+'use strict';
+
+try {
+  var mongoose = require('mongoose');
+} catch (_) {
+  // workaround when `npm link`'ed for development
+  var prequire = require('parent-require')
+    , mongoose = prequire('mongoose');
+}
+
+var Boom = require('boom'),
+  User = mongoose.model('User');
+
+module.exports = {
+  update: function(req, rep){
+    User.update({_id: req.auth.credentials.id}, {$addToSet: {favorites: req.params.id}}, {multi: false}, function(err,user){
+      if( err ) { return rep( Boom.badRequest(err) ); }
+
+      return rep({update: true});
+    });
+  },
+  delete: function(req, rep){
+    User.update({_id: req.auth.credentials.id}, {$pull: {favorites: req.params.id}}, {multi: false}, function(err,user){
+      if( err ) { return rep( Boom.badRequest(err) ); }
+
+      return rep({update: true});
+    });
+  }
+}
